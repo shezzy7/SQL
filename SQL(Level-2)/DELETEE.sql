85fb20171c395832129db11e37a4253c62d42042
Shahzad Hussain <shahzadhussain72242@gmail.com>
2025-06-11 15:52:00 +0500

    Update DELETEE.sql

--- SQL(Level-2)/DELETEE.sql
+++ SQL(Level-2)/DELETEE.sql
@@ -8,5 +8,5 @@
 	   	('E003','Kamran Ali','Ali Ahmad',24,75000,'D002'),('E004','Kashif Ali','Ali Kamran',25,80000,'D001'),('E005','Hina Munir','Munir
  	   	Ahamad',22,65000,'D004'),('E006','Hamna Ali','Hassan Ali',27,75000,'D002'),('E007','Obaid Ali','Ali Ahmad',32,90000,'D001'),  
         		('E008','Muhammad Kamran','Kamran Ali',35,95000,'D004'),('E009','Muhammad Ali','Ali Hassan',21,55000,'D001');
-		--let we want to DELETE data of all the employee whose age is greater then
+		--let we want to DELETE data of all the employee whose age is greater then 30
 		DELETE FROM employee WHERE empAge>=30; 
